581d7ed38b74a3dd3d9db53b909cbcd1f5f6ca7d
spectatorMrZ <zhuchunan001@sina.com>
2024-08-08 01:49:32 +0800

    fix: rpc interceptor use default lang when not pass accept lang on header

--- goi18nx/rpc_interceptor.go
+++ goi18nx/rpc_interceptor.go
@@ -5,10 +5,15 @@ import (
 	i18n2 "github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
+	"log"
 )
+
+var rpcInterceptorDebug = false
+
+func WithRpcInterceptorDebug(debug bool) {
+	rpcInterceptorDebug = debug
+}
 
 type I18nGrpcInterceptor struct {
 	supportTags       []language.Tag
@@ -16,6 +21,9 @@ type I18nGrpcInterceptor struct {
 }
 
 func NewI18nGrpcInterceptor(supportTags []language.Tag, localizationFiles []string) *I18nGrpcInterceptor {
+	if len(supportTags) == 0 {
+		panic("supportTags can not be empty")
+	}
 	return &I18nGrpcInterceptor{
 		supportTags:       supportTags,
 		localizationFiles: localizationFiles,
@@ -32,15 +40,29 @@ func (i *I18nGrpcInterceptor) Interceptor(ctx context.Context, req interface{}, 
 
 func (i *I18nGrpcInterceptor) saveLocalize(ctx context.Context) (context.Context, error) {
 	respHeader, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, status.Error(codes.Code(defaultErrCode), "can not correct get metadata1")
+	//lang := "en-US" //language.English
+	langTag := i.supportTags[0]
+	lang := langTag.String()
+	if ok {
+		langs := respHeader.Get(defaultLangHeaderKey)
+		if len(langs) == 0 {
+			//return nil, status.Error(codes.Code(defaultErrCode), "can not correct get language")
+			if rpcInterceptorDebug {
+				log.Printf("can not correct get language")
+			}
+		} else {
+			lang = langs[0]
+			langTag = MatchCurrentLanguageTag(lang, i.supportTags)
+		}
+	} else {
+		if rpcInterceptorDebug {
+			log.Printf("can not correct get metadata1")
+		}
+		//return nil, status.Error(codes.Code(defaultErrCode), "can not correct get metadata1")
 	}
-	langs := respHeader.Get(defaultLangHeaderKey)
-	if len(langs) == 0 {
-		return nil, status.Error(codes.Code(defaultErrCode), "can not correct get language")
-	}
-	lang := langs[0]
-	langTag := MatchCurrentLanguageTag(lang, i.supportTags)
+
+	//lang := langs[0]
+	//langTag := MatchCurrentLanguageTag(lang, i.supportTags)
 	bundle := NewBundle(langTag, i.localizationFiles...)
 	localizer := i18n2.NewLocalizer(bundle, lang)
 	ctx = setLocalizer(ctx, localizer)
